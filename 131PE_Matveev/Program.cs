d4a38f3f4bcfd55b9004641c3c5ae95116472e22
Matveev_Aleksei <A.matveev1995@mail.ru>
2016-02-01 11:13:18 +0300

    Пустой проект

---
+++ 131PE_Matveev/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _131PE_Matveev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
