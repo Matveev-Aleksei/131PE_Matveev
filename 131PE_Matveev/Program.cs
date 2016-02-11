fbd67276c3d4a3ef27473a4a106eb8c553d33ed5
Matveev-Aleksei <A.matveev1995@mai.ru>
2016-02-11 14:50:52 +0300

    Вычисление зп по кол-ву дней работы

--- 131PE_Matveev/Program.cs
+++ 131PE_Matveev/Program.cs
@@ -13,37 +13,60 @@ namespace _131PE_Matveev
             // Задание 1. Приложение для расчета ЗП.
            
             int i;
+            int d;
             int z;
-            z = 0;
-            Console.WriteLine("Введите размер дневной выручки ");
-            i = Convert.ToInt32(Console.ReadLine());
+            int c = 0;
+            int a = 0;
 
-            if (i <= 1000)
+            Console.WriteLine("Введите колличество дней: ");
+            d = Convert.ToInt32(Console.ReadLine());
+
+            if (d > 5)
+            {
+                Console.WriteLine("Напоминание! Работа в выходные дни оплачивается по двойному тарифу");
+            }
+
+                do
                 {
-                    z = i / 100 * 5;
-                    Console.WriteLine("Размер выручки равен ");
-                    Console.Write(z); 
+                    Console.WriteLine("Введите размер дневной выручки: ");
+                    i = Convert.ToInt32(Console.ReadLine());
+
+                    if (i <= 10000)
+                        {
+                            z = i * 5 / 100;
+                            Console.WriteLine("Размер заработной платы равен: " + z);
+                        }
+
+                    else
+                        {
+                            z = i * 10 / 100 + 500;
+                            Console.WriteLine("Размер заработной платы равен: " + z);
+                        }
+
+                    if (z < 300)
+                        {
+                            z = 300;
+                            Console.WriteLine("Минимальный размер оплаты труда за 1 день равен 300р. ");
+                        }
+
+                    a++;
+                    
+
+                    if (a > 5)
+                        {
+                            z = z * 2;
+                            Console.WriteLine("Внимание! Двойной тариф. Заработная плата за этот день равна: " + z);
+                        }
+
+                    c = c + z;
+
+                    if (a == d)
+                        {
+                            Console.WriteLine("Заработная плата составляет: " + c);
+                        }
                 }
-
-            if (i > 10000)
-                {
-                
-                    z = i / 100 * 10 + 500;
-                    Console.WriteLine("Размер выручки равен ");
-                    Console.Write(z);
-                }
-
-
-             // Задание 2. Введение понятия - минимальная ЗП. 
-
-            if (z < 300)
-                {
-                    Console.WriteLine("Внимание! Минимальный размер оплаты труда за день = 300р");
-                    z = 300;
-                    Console.WriteLine("Размер выручки равен ");
-                    Console.Write(z);
-                }
-
+                while (a < d);
+            
            
 
             Console.ReadKey();
